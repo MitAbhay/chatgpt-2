5e7ca7be820bafff47d7510f1c858eba8fdda64e
mitabhay <leader.abhay002@gmail.com>
2023-01-30 22:45:56 +0530

    Bug Fixed

--- main.go
+++ main.go
@@ -10,59 +10,56 @@
 	 "github.com/spf13/cobra"
  )
 
- func GetResponse(client *gpt3.Client, cntxt context.Context, question string) {
-	err := client.CompletionStreamWithEngine(ctx , gpt3.TextDavinci003Engine , gpt3.CompletionRequest{
+ func GetResponse(client gpt3.Client, cntxt context.Context, question string) {
+	err := client.CompletionStreamWithEngine(cntxt , gpt3.TextDavinci003Engine , gpt3.CompletionRequest{
 		Prompt: []string{
-			question
+			question,
 		},
-		MaxTokens : gpt3.IntPtr(3000)
-		// Stop : []string{.}
-		Temperature : gpt3.Float32Ptr(0)
-		func(resp *gpt3.CompletionResponse, err error) {
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(resp.Choices[0].Text)
-		}
+		MaxTokens : gpt3.IntPtr(3000),
+		Temperature : gpt3.Float32Ptr(0),
+		},
+		func(resp *gpt3.CompletionResponse){
+		fmt.Println(resp.Choices[0].Text)
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
  }
 
- type NullWriter int 
+ type NullWriter int
 
  func (NullWriter) Write([]byte) (int, error) { return 0, nil}
 
  func main() {
-    log.SetOutput(new(NullWriter))
-	viper.SetConfigFile('.env');
+    // log.SetOutput(new(NullWriter))
+	viper.SetConfigFile(".env")
 	viper.ReadInConfig();
 	apikey := viper.GetString("API_KEY");
-
 	if(apikey == "") {
 		panic("API_KEY not found in .env")
 	}
-
 	cntxt := context.Background()
 	client := gpt3.NewClient(apikey)
-	rootCmd :=&Cobra.Command{
+	rootCmd :=&cobra.Command{
 		Use: "gpt3",
 		Short: "ChatGPT in console",
 		Run : func(cmd *cobra.Command, args []string) {
 			scanner := bufio.NewScanner(os.Stdin)
 			quit := false
-
 			for !quit {
-				fmt.Print("Say something: ")
+				fmt.Print("ChatGPT in console : : ")
 				scanner.Scan()
 				question := scanner.Text()
 				switch question {
 				case "quit":
 					quit = true
-
 				default:
 					GetResponse(client,cntxt,question)
 				}
 			}
-
-		}
+		},
 	}
 	rootCmd.Execute()
  }
